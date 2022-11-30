0361b6de94e359db4c1a79914ece6b9c024a722b
Shachar97 <shachar.ketz@gmail.com>
2022-11-30 20:43:16 +0200

    start coding: only shield

--- my_mat.c
+++ my_mat.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
- 
+#include <cstdbool>
+#include <my_mat.h>
+
+#define MTRITION_NUM 10
+
+
+
+static int isPathMatbuild = false;//change when we scan 'A'
+static int mat[MTRITION_NUM][MTRITION_NUM]={0};
+
 int exsistpath(int a, int b){
-
+    if(!isPathMatbuild){
+        int buildPathMat();//Todo
+        isPathMatbuild = true;
+    }
+    if (shortPath(a,b)!=0){
+        return 1;
+    }
+    return 0;
  }
- int shortpath(int c ,int d){
-
+ int shortPath(int c ,int d){
+    if(!isPathMatbuild){
+        int buildPathMat();//Todo
+        isPathMatbuild = true;
+    }
+ }
+ int building(){
+    printf("in a");
+        
+        int x;
+        for (size_t i = 0; i < MTRITION_NUM; i++)
+        {
+            for (size_t j = 0; j < MTRITION_NUM; j++)
+            {
+                /* to inilaize the mat */
+                mat[i][j]= scanf("%d",&x);
+            }
+        }
+        break;
  }
