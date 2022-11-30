d3ae8a8550b26c3478fc2783b46ecf0d3f241d28
Shachar97 <shachar.ketz@gmail.com>
2022-11-30 20:42:06 +0200

    adding the headers

---
+++ my_mat.h
@@ -0,0 +1,4 @@
+int exsistpath(int a, int b);//return 1 if exsist and 0 atherwise
+int shortPath(int c ,int d);//return the short path between A __ and B
+int building();/*header for matrition, get NxN numbers from the scanner */ 
+int buildPathMat();//counculate the short path between any ___
